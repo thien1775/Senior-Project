b070ed1b42a86e4c77f61603f7461b5cfcfa48b6
thien1775 <thien1775@yahoo.com>
2018-03-15 19:28:42 -0700

    Create proto.sh

---
+++ proto.sh
@@ -0,0 +1,12 @@
+#!/bin/sh -e
+sudo apt-get update -y
+sudo apt-get upgrade -y
+git clone https://github.com/google/protobuf.git
+cd protobuf
+sudo apt-get install autoconf automake libtool curl make g++ unzip
+./autogen.sh 
+./configure 
+make -j2
+make check -j2
+sudo make install -j2
+sudo ldconfig
